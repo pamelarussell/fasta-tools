a264e57dcb04c2c904b3ce9b9ca8771852eb75f1
Pamela Russell <pamela.russell@gmail.com>
2019-04-08 10:47:03 -0600

    fix behavior for first line of fasta file

--- fasta_seq_lens.sh
+++ fasta_seq_lens.sh
@@ -17,7 +17,8 @@ fasta=$1
 cat $fasta | \
 awk '
   BEGIN {name = ""; len=0}
-  $1 ~ /^>/ && NR > 0 {print name "\t" len; name = $1; gsub("^>", "", name); len = 0}
+  $1 ~ /^>/ && NR == 1 {name = $1; gsub("^>", "", name); len = 0}
+  $1 ~ /^>/ && NR > 1 {print name "\t" len; name = $1; gsub("^>", "", name); len = 0}
   $1 !~ /^>/ {len = len + length($1)}
   END {print name "\t" len}
 '
