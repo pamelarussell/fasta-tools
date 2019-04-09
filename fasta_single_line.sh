504263326cedc709c0d5f1f2b6863d94491eab7d
Pamela Russell <pamela.russell@gmail.com>
2019-04-09 15:24:05 -0600

    allow a minimum seq len to include

--- fasta_single_line.sh
+++ fasta_single_line.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
-help_str="\n-----------------\nfasta_single_line.sh\n-----------------\n\nUsage: fasta_single_line.sh <input_fasta_file> <output_fasta_file>\n"
+help_str="\n-----------------\nfasta_single_line.sh\n-----------------\n\nUsage: fasta_single_line.sh <input_fasta_file> <output_fasta_file> <optional_min_len_to_include_seq>\n"
 
 if [ "$1" == "-h" ]; then
   echo -e $help_str
-  exit -1
+  exit 0
 fi
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -lt 2 ]; then
     echo -e $help_str
     exit -1
+fi
+
+if [ "$#" -gt 2 ]; then
+  min_len=$3
+else
+  min_len=0
 fi
 
 input=$1
 output=$2
 
 cat $input | \
-awk '
+awk -v min_len=$min_len '
   BEGIN {header = ""; seq = ""}
   $1 ~ /^>/ && NR == 1 {header = $1; seq = ""}
-  $1 ~ /^>/ && NR > 1 {print header "\n" seq; header = $1; seq = ""}
+  $1 ~ /^>/ && NR > 1 {
+    if(length(seq) >= min_len) {
+      print header "\n" seq
+    }; header = $1; seq = ""
+  }
   $1 !~ /^>/ {seq = seq $1}
-  END {print header "\n" seq}
+  END {if(length(seq) >= min_len) {
+    print header "\n" seq
+  }}
 ' > $output
